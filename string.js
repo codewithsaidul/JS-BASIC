09429bd098089c5a1aafd6fd1af0fa86bd07ab62
codewithsaidul <codewithsaidul@gmail.com>
2024-01-30 20:46:23 +0600

    19.1 & 19.2 Video Added

---
+++ string.js
@@ -0,0 +1,30 @@
+// Video 19-1 
+
+// const propose = `"I Love You Kulsuma"`;
+
+// const sado = new String ("I Love You Kulsuma")
+
+// console.log(typeof propose)
+
+// console.log(typeof sado)
+
+
+const school = 'RAJ UK Uttara Model School';
+
+console.log(school);
+
+console.log(school.toLowerCase());
+
+console.log(school.toUpperCase())
+
+
+const sub = 'Chemistry';
+
+const book = 'chemistry';
+
+
+if (sub === book) {
+    console.log("I am Reading book")
+} else {
+    console.log("Hudai Pista Ultay")
+}
